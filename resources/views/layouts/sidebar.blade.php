4139542c0d7e5583fad3f503e8a5409e25ee90d2
Rahmania Jasmin <fararahmania5@gmail.com>
2020-01-14 23:18:06 +0700

    running the app

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,15 +15,15 @@
 
                     {{-- Menu Dosen--}}
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.dosen.index') }}">
+                        <a class="nav-link" href="">
                             <i class="nav-icon"></i>Dosen
                         </a>
                     </li>
 
                     {{-- Menu Mahasiswa--}}
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.mahasiswa.index') }}">
-                            <i class="nav-icon"></i> Mahasiswa
+                        <a class="nav-link" href="">
+                            <i class="nav-icon"></i>Mahasiswa
                         </a>
                     </li>
 
